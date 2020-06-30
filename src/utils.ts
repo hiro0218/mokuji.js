b3ca4229dc48aaeb4cb055b08214df43d6c3729e
hiro <hiro0218@gmail.com>
2020-06-30 15:27:21 +0900

    add: getHeadingTagName2Number

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
 export const replaceSpace2Underscore = (text: string) => {
   return text.replace(/\s+/g, "_");
 };
+
+export const getHeadingTagName2Number = (tagName: string) => {
+  // @ts-ignore
+  const currentNumber = tagName.match(/\d/g).join("");
+
+  return Number(currentNumber);
+};
