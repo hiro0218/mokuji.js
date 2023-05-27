ec343ff89827109e33d3e678bfde6ee1d95f7bf8
hiro0218 <hiro0218@gmail.com>
2023-05-28 00:45:23 +0900

    add: type module

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:unicorn/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   overrides: [],
@@ -11,7 +12,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['unicorn', '@typescript-eslint'],
-  ignorePatterns: ['node_modules', 'dist', '.eslintrc.js'],
+  ignorePatterns: ['node_modules', 'dist', '.eslintrc.js', 'tsup.config.ts'],
   rules: {
     'unicorn/consistent-destructuring': 'off',
     'unicorn/filename-case': 'off',
