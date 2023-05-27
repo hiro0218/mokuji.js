a84f012eda5fd6f6d7bbf2045d16818fb2d10700
hiro0218 <hiro0218@gmail.com>
2023-05-28 00:15:35 +0900

    fix: bundle setting

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => {
   return {
+    entry: ['src/index.ts'],
     target: 'es2020',
     format: ['cjs', 'esm'],
     sourcemap: true,
     minify: !options.watch,
+    treeshake: true,
     clean: true,
     dts: true,
   };
