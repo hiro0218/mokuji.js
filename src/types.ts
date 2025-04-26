b058de82a808633daf7487583922e4b4346554ea
hiro <hiro0218@gmail.com>
2025-04-27 01:58:29 +0900

    feature: Add minLevel and maxLevel to MokujiOption type and adjust heading filtering

--- src/types.ts
+++ src/types.ts
@@ -9,4 +9,8 @@ export type MokujiOption = {
   anchorLinkPosition?: 'before' | 'after';
   anchorLinkClassName?: string;
   anchorContainerTagName?: AnchorContainerTagNameProps;
+  /** 最小見出しレベル（例: 1はh1を意味する。これより小さいレベルは表示しない） */
+  minLevel?: number;
+  /** 最大見出しレベル（例: 3はh3を意味する。これより大きいレベルは表示しない） */
+  maxLevel?: number;
 };
