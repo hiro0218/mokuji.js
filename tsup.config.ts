8276b081592bcab7f664ccf58f04cfd7ac631a46
hiro <hiro0218@gmail.com>
2025-07-12 20:24:05 +0900

    feat: Add CommonJS format support and improve esbuild options in tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,11 +4,17 @@ export default defineConfig((options) => {
   return {
     entry: ['src/index.ts'],
     target: 'es2024',
-    format: ['esm'],
+    format: ['esm', 'cjs'],
     sourcemap: true,
     minify: !options.watch,
     treeshake: true,
     clean: true,
     dts: true,
+    splitting: false,
+    esbuildOptions(options) {
+      options.banner = {
+        js: '/**\n * mokuji.js - A table of content JavaScript Library\n * @license MIT\n * @author hiro\n * @repository https://github.com/hiro0218/mokuji.js\n */',
+      };
+    },
   };
 });
