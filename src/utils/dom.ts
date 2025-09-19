fa227c4b873e73cc7651fbc48d98e616f56f65ad
hiro <hiro0218@gmail.com>
2025-09-20 01:05:12 +0900

    refactor: Remove element caching from createElement for simplicity

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -3,15 +3,8 @@ export const getAllHeadingElements = (containerElement: Element): HTMLHeadingEle
   return [...headings] as HTMLHeadingElement[];
 };
 
-const elementCache = new Map<string, HTMLElement>();
-
 export const createElement = <T extends keyof HTMLElementTagNameMap>(tagName: T): HTMLElementTagNameMap[T] => {
-  if (!elementCache.has(tagName)) {
-    elementCache.set(tagName, document.createElement(tagName));
-  }
-
-  const cachedElement = elementCache.get(tagName)!;
-  return cachedElement.cloneNode(false) as HTMLElementTagNameMap[T];
+  return document.createElement(tagName);
 };
 
 export const removeAllElements = (elements: NodeListOf<Element> | Element[]): void => {
